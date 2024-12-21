a107d8f513081a51c42a9c5e3ec8d28643af93b5
Anderson Battisti <anderson.battisti@universo.univates.br>
2024-12-21 17:31:29 -0300

    Post endpoint added to the API. @PathVariable added to pass a parameter in the URL, @RequestParam added to handle a filter passed in the URL and @RequestBody added to receive a JSON through the request body with the name and the email of the class User. Package domain added and the class User was created there, the class User has 'invisible' methods created by the lombok plugin using @Getters and @Setter

--- src/main/java/com/battisti/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/battisti/first_spring_app/controller/HelloWorldController.java
@@ -1,16 +1,26 @@
 package com.battisti.first_spring_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.battisti.first_spring_app.domain.User;
+import com.battisti.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController
 {
+    @Autowired
+    private HelloWorldService helloWorldService;
+
     @GetMapping
     public String helloWorld()
     {
-        return "Hello World";
+        return helloWorldService.helloWorld("Anderson");
+    }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id, @RequestParam(value = "filter", defaultValue = "nenhum") String filter, @RequestBody User body)
+    {
+        return "Hello World Post: " + body.getName() + " | Filter = " + filter + " Id = " + id;
     }
 }
